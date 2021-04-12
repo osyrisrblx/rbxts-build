8eb879509c4ba9149801e65f4e02e1ade5845d5b
osyrisrblx <osyrisrblx@gmail.com>
2021-04-11 17:09:01 -0700

    Fix usage from Windows

--- src/util/run.ts
+++ src/util/run.ts
@@ -1,13 +1,14 @@
 import chalk from "chalk";
-import { spawn } from "child_process";
+import { spawn, SpawnOptions } from "child_process";
 import { CLIError } from "../errors/CLIError";
 
 export function run(command: string, args: ReadonlyArray<string> = []) {
 	return new Promise<void>((resolve, reject) => {
 		console.log(chalk.yellow(command, ...args));
-		const childProcess = spawn(command, args);
-		childProcess.stdout.on("data", data => process.stdout.write(data));
-		childProcess.stderr.on("data", data => process.stderr.write(data));
+		const options: SpawnOptions = { shell: true };
+		const childProcess = spawn(command, args, options);
+		childProcess.stdout?.on("data", data => process.stdout.write(data));
+		childProcess.stderr?.on("data", data => process.stderr.write(data));
 		childProcess.on("error", err => {
 			throw err;
 		});
