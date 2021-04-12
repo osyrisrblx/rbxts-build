5ed4bae6cc985a3b947354093f7b98e136c7bb4d
osyrisrblx <osyrisrblx@gmail.com>
2021-04-11 18:14:25 -0700

    Fix packageJsonType definition

--- src/typeChecks.ts
+++ src/typeChecks.ts
@@ -1,18 +1,23 @@
 import * as z from "zod";
 
-export const packageJsonType = z.object({
-	scripts: z.object({
-		build: z.string().optional(),
-		start: z.string().optional(),
-		stop: z.string().optional(),
-	}),
-	"rbxts-build": z.object({
-		rbxtscArgs: z.array(z.string()).optional(),
-		rojoBuildArgs: z.array(z.string()).optional(),
-		syncLocation: z.string().optional(),
-		wslUseExe: z.boolean().optional(),
-		dev: z.boolean().optional(),
-	}),
-});
+export const packageJsonType = z
+	.object({
+		scripts: z.object({
+			compile: z.string().optional(),
+			build: z.string().optional(),
+			open: z.string().optional(),
+			start: z.string().optional(),
+			stop: z.string().optional(),
+			sync: z.string().optional(),
+		}),
+		"rbxts-build": z.object({
+			rbxtscArgs: z.array(z.string()).optional(),
+			rojoBuildArgs: z.array(z.string()).optional(),
+			syncLocation: z.string().optional(),
+			wslUseExe: z.boolean().optional(),
+			dev: z.boolean().optional(),
+		}),
+	})
+	.nonstrict();
 
 export type packageJsonType = z.infer<typeof packageJsonType>;
