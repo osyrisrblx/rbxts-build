e76d0f9b98a85378bc5d2b5551b5bf5f28ecbea8
osyrisrblx <osyrisrblx@gmail.com>
2022-06-15 12:45:48 -0400

    Fix getWindowsPath bugs

--- src/util/getWindowsPath.ts
+++ src/util/getWindowsPath.ts
@@ -1,11 +1,5 @@
 import { cmd } from "./cmd";
-import { Lazy } from "./Lazy";
-
-const rootWslPath = new Lazy(async () => {
-	return (await cmd(`wslpath -w /`)).trim();
-});
 
 export async function getWindowsPath(fsPath: string) {
-	const windowsPath = (await rootWslPath.get()) + fsPath.replace(/\\/g, "\\\\").replace(/\//g, "\\").slice(1);
-	return windowsPath.replace(/\\/g, "\\\\");
+	return (await cmd(`wslpath -w ${fsPath}`)).trim().replace(/\\/g, "\\\\").replace(/\//g, "\\");
 }
