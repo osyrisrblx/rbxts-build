957263ddb76d7f76077a465d37b88c840baaa321
osyrisrblx <osyrisrblx@gmail.com>
2023-10-15 00:25:43 -0400

    Remove deprecated .nonstrict()

--- src/typeChecks.ts
+++ src/typeChecks.ts
@@ -1,18 +1,16 @@
 import * as z from "zod";
 
-export const packageJsonType = z
-	.object({
-		"rbxts-build": z
-			.object({
-				rbxtscArgs: z.array(z.string()).optional(),
-				rojoBuildArgs: z.array(z.string()).optional(),
-				syncLocation: z.string().optional(),
-				wslUseExe: z.boolean().optional(),
-				dev: z.boolean().optional(),
-				watchOnOpen: z.boolean().optional(),
-			})
-			.optional(),
-	})
-	.nonstrict();
+export const packageJsonType = z.object({
+	"rbxts-build": z
+		.object({
+			rbxtscArgs: z.array(z.string()).optional(),
+			rojoBuildArgs: z.array(z.string()).optional(),
+			syncLocation: z.string().optional(),
+			wslUseExe: z.boolean().optional(),
+			dev: z.boolean().optional(),
+			watchOnOpen: z.boolean().optional(),
+		})
+		.optional(),
+});
 
 export type packageJsonType = z.infer<typeof packageJsonType>;
