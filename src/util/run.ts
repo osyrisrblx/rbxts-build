bf37d8b1d8f7e5485588b8111fc87ee05bfb2816
Osyris <osyrisrblx@gmail.com>
2025-08-13 14:38:10 -0400

    shell: false test

--- src/util/run.ts
+++ src/util/run.ts
@@ -5,8 +5,7 @@ import { CLIError } from "../errors/CLIError";
 export function run(command: string, args: ReadonlyArray<string> = []) {
 	return new Promise<void>((resolve, reject) => {
 		console.log(kleur.yellow(command), ...args.map(kleur.yellow));
-		const options: SpawnOptions = { shell: true };
-		const childProcess = spawn(command, args, options);
+		const childProcess = spawn(command, args, { shell: false });
 		childProcess.stdout?.on("data", data => process.stdout.write(data));
 		childProcess.stderr?.on("data", data => process.stderr.write(data));
 		childProcess.on("error", err => {
