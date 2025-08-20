e305e1c75b5703aed0041ce737404ef063b40791
Osyris <osyrisrblx@gmail.com>
2025-08-19 22:54:56 -0400

    Update run utility to inherit stdio from child process

--- src/util/run.ts
+++ src/util/run.ts
@@ -5,7 +5,7 @@ import { CLIError } from "../errors/CLIError";
 export function run(command: string, args: ReadonlyArray<string> = []) {
 	return new Promise<void>((resolve, reject) => {
 		console.log(kleur.yellow(command), ...args.map(kleur.yellow));
-		const childProcess = spawn([command, ...args].join(" "), { shell: true });
+		const childProcess = spawn([command, ...args].join(" "), { shell: true, stdio: "inherit" });
 		childProcess.stdout?.on("data", data => process.stdout.write(data));
 		childProcess.stderr?.on("data", data => process.stderr.write(data));
 		childProcess.on("error", err => {
